5d852183bdcb7146f6e67c3bf88636eb0b71e223
吴小青 <wuxiaoqing_44272@wdai.com>
2018-10-14 14:55:44 +0800

    修改版本问题

--- rcp-sample/sample-produce/src/main/java/com/wu/DemoApplication.java
+++ rcp-sample/sample-produce/src/main/java/com/wu/DemoApplication.java
@@ -3,6 +3,8 @@ package com.wu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class DemoApplication {
 
